0bfab817a7f8edc495f8f34dab5e3a126b62c716
Marc Ferraro <marc.ferraro1@gmail.com>
2020-11-25 20:58:12 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,25 @@
+def reverse_each_word (string)
+ stored_string = []
+ reversed_string = []
+ stored_string = string.split
+ 
+ stored_string.each do |word|
+   reversed_string << word.reverse
+   
+ end
+final_string = reversed_string.join(" ")
+
+end
+
+def reverse_each_word (string)
+  stored_string = []
+ reversed_string = []
+ stored_string = string.split
+ 
+ stored_string.collect do |word|
+   reversed_string << word.reverse
+   
+ end
+final_string = reversed_string.join(" ")
+
+end
