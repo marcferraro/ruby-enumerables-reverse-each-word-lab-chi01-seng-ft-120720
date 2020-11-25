5a5a85f6ef9bedc50ef9900b7d69ec66ae80aaa2
Marc Ferraro <marc.ferraro1@gmail.com>
2020-11-25 20:53:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word (string)
   
   stored_string = []
   reversed_string = []
+  returned_string = nil
  stored_string = string.split
  
  stored_string.each do |word|
@@ -9,6 +10,6 @@ def reverse_each_word (string)
    
  end
 final_string = reversed_string.to_s
-final_string.join(" ")
-final_string
+returned_string = final_string.join(" ")
+returned_string
 end
